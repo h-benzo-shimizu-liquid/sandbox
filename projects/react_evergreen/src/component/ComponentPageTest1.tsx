5f3f1747ee507537e2a4fc1bca0e829cf2af4882
h-benzo-shimizu-liquid <h.shimizu@liquidinc.asia>
2021-11-19 10:52:40 +0900

    evergreen テスト

--- projects/react_evergreen/src/component/ComponentPageTest1.tsx
+++ projects/react_evergreen/src/component/ComponentPageTest1.tsx
@@ -4,9 +4,15 @@
 // ----------------------------------------------------------------
 
 import * as React from "react";
+import { Button, Pane, Text, majorScale } from 'evergreen-ui'
 
 const Component: React.FunctionComponent<{}> = (): JSX.Element => {
-	return <div>test1</div>;
+	return <div>
+		<Pane display="flex" alignItems="center" marginX={majorScale(2)}>
+			<Button>Click me!</Button>
+			<Text>This is a clickable Button</Text>
+		</Pane>
+	</div>;
 };
 
 export default Component;
