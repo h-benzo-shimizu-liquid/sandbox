a5b9aa99c1088fbd13ce2aa6f0669ece335f8de8
h-benzo-shimizu-liquid <h.shimizu@liquidinc.asia>
2021-11-22 17:23:46 +0900

    プロジェクトのコピー

---
+++ projects/react_chakra/webpack.config.base.js
@@ -0,0 +1,42 @@
+
+// ----------------------------------------------------------------
+// ----------------------------------------------------------------
+// ----------------------------------------------------------------
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+// ----------------------------------------------------------------
+// ----------------------------------------------------------------
+// ----------------------------------------------------------------
+
+module.exports = {
+	entry: path.resolve(__dirname, "./src/main.ts"),
+	output: {
+		path: path.resolve(__dirname, "./dist"),
+		filename: "index.js?[hash]",
+	},
+	resolve: {
+		extensions: [".js", ".ts", ".tsx",],
+		alias: { "@": path.resolve(__dirname, "src"), },
+	},
+	module: {
+		rules: [{
+			test: /\.(ts|tsx)$/,
+			loader: "ts-loader",
+		},],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({ template: path.join(__dirname, "src/index.html"), }),
+	],
+	devServer: {
+		static: {
+			directory: path.resolve(__dirname, "./dist"),
+		},
+	},
+};
+
+// ----------------------------------------------------------------
+// ----------------------------------------------------------------
+// ----------------------------------------------------------------
+
