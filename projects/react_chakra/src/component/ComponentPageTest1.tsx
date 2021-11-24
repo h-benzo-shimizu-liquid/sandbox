d77cc3c6e1883bed1f69c96b9764bee66a154f02
h-benzo-shimizu-liquid <h.shimizu@liquidinc.asia>
2021-11-24 13:48:23 +0900

    chakra test

--- projects/react_chakra/src/component/ComponentPageTest1.tsx
+++ projects/react_chakra/src/component/ComponentPageTest1.tsx
@@ -4,9 +4,20 @@
 // ----------------------------------------------------------------
 
 import * as React from "react";
+import {
+  ChakraProvider,
+  Box,
+  Stack,
+} from "@chakra-ui/react"
 
 const Component: React.FunctionComponent<{}> = (): JSX.Element => {
-	return <div>test1</div>;
+  return <ChakraProvider>
+    <Stack m={2}>
+      <Box bg="red.50" p={2}>a</Box>
+      <Box bg="red.50" p={2}>b</Box>
+      <Box bg="red.50" p={2}>c</Box>
+    </Stack>
+  </ChakraProvider>;
 };
 
 export default Component;
