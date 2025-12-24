c86fcf38a20cbd82f91cfa4508aeb80e41b2cfb2
h-benzo-shimizu-liquid <h.shimizu@liquidinc.asia>
2025-12-24 12:06:51 +0900

    古い鍵を新しく

--- bin/createCertificate.sh
+++ bin/createCertificate.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-echo ////////////////////////////////////////////////////////////////
-echo ////////////////////////////////////////////////////////////////
-echo ////////////////////////////////////////////////////////////////
-echo passphraseはtestが良いな
-echo ////////////////////////////////////////////////////////////////
-echo ////////////////////////////////////////////////////////////////
-echo ////////////////////////////////////////////////////////////////
-
 DIR=$(cd $(dirname $0)/../bin; pwd)
 FILENAME1=${DIR}/mysslserver.key
 FILENAME2=${DIR}/mysslserver.csr
@@ -17,4 +9,4 @@ FILENAME4=${DIR}/mysslserver.pfx
 openssl genrsa -out ${FILENAME1} 2048
 openssl req -new -key ${FILENAME1} -out ${FILENAME2} -subj "/C=JP/ST=Tokyo/L=Tokyo/O=Example Ltd./OU=Web/CN=example.com"
 openssl x509 -req -signkey ${FILENAME1} -in ${FILENAME2} -out ${FILENAME3} -days 3650
-openssl pkcs12 -export -inkey ${FILENAME1} -in ${FILENAME3} -out ${FILENAME4}
+openssl pkcs12 -export -inkey ${FILENAME1} -in ${FILENAME3} -out ${FILENAME4} -keypbe AES-256-CBC -certpbe AES-256-CBC -passout pass:test
